686174a13b55a45eef197621d5a6ec8a76fe5af2
Crisley Linhares <prestes40@gmail.com>
2023-02-23 17:51:35 -0400

    Aula 8 - Testando login

---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,15 @@
+Cypress.Commands.add('login', (
+    user = Cypress.env('user_name'),
+    password = Cypress.env('user_password'),
+) => {
+    const login = () => {
+        cy.visit('/users/sign_in')
+        cy.get('#user_login')
+            .type(user)
+        cy.get('#user_password')
+            .type(password, { log:false })
+        cy.get('input[data-qa-selector="sign_in_button"]')
+            .click()
+    }
+    login()
+})
